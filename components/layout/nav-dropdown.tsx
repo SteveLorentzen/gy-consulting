4a17016cbd24a6d85a8ef81d52829775a7cd76e8
Steve Lorentzen <Stevelorentzen@gmail.com>
2022-01-22 23:20:17 -0800

    created a layout, header, nav, nav-dropdown, and nav-mobile component worked on responsive design of header created a constants file called my-constants.ts to have both menus draw from the same data source created an interfaces-and-types file created dummy content for about-us page to make sure links to anchor tags were working

---
+++ components/layout/nav-dropdown.tsx
@@ -0,0 +1,50 @@
+import { Menu, MenuList, MenuButton, MenuItem } from '@reach/menu-button'
+import { IDropdown } from 'interfaces-and-types'
+import { navTextStyling } from 'my-constants'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+export const NavDropdown: React.FC<IDropdown> = ({
+  dropdownTitle,
+  dropdownOptions,
+  mainDestination,
+}) => {
+  const router = useRouter()
+
+  return (
+    <Menu>
+      {({ isOpen }) => (
+        <>
+          <MenuButton
+            id={mainDestination.slice(1)}
+            className={`${navTextStyling}  ${isOpen ? 'text-green-400' : ''} ${
+              router.pathname === mainDestination ? 'text-green-400' : ''
+            }`}
+          >
+            {dropdownTitle} <span aria-hidden>▾</span>
+          </MenuButton>
+          <MenuList className="flex flex-col z-20 relative bg-white rounded shadow-md animate-descend">
+            {dropdownOptions.map(navLink => {
+              return (
+                <MenuItem
+                  key={navLink.linkText}
+                  // requires onSelect prop but it isn't needed
+                  onSelect={() => {
+                    return
+                  }}
+                  className="flex items-stretch h-9 hover:cursor-pointer hover:bg-slate-50 px-3 "
+                >
+                  <Link href={navLink.destination}>
+                    <a className="flex items-center w-full ">
+                      {navLink.linkText}
+                    </a>
+                  </Link>
+                </MenuItem>
+              )
+            })}
+          </MenuList>
+        </>
+      )}
+    </Menu>
+  )
+}
--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -1,3 +1,15 @@
 export default function AboutUsPage() {
-  return <h1>About Us</h1>
+  return (
+    <div>
+      <div className="w-full bg-green-300 min-h-screen" id="who-we-are">
+        Who We Are
+      </div>
+      <div className="w-full bg-blue-300 min-h-screen" id="why-gy-consulting">
+        Why GY Consulting
+      </div>
+      <div className="w-full bg-red-300 min-h-screen" id="the-team">
+        The Team
+      </div>
+    </div>
+  )
 }
