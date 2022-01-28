8b6c4ecc4bc51bccfe93d8de621eb142762de436
Steve Lorentzen <Stevelorentzen@gmail.com>
2022-01-28 14:43:00 -0800

    resolved merge conflicts

--- screens/gy-blog/gy-blog.tsx
+++ screens/gy-blog/gy-blog.tsx
@@ -115,58 +115,81 @@ const BlogPost: React.FC<{ blog: IBlog }> = ({ blog }) => {
 export function GYBlogPage() {
   return (
     <>
-      <section className="flex justify-center items-center bg-gradient-to-l from-cyan-900 to-cyan-700">
-        <h1 className="flex flex-col justify-end text-white text-5xl sm:text-6xl lg:text-7xl h-24 sm:h-28 md:h-32 lg:h-40 p-6">
-          GY Blog
-        </h1>
-      </section>
-      <div className="flex flex-col md:flex-row justify-between w-full sm:w-11/12 max-w-screen-2xl mx-auto mt-8 lg:p-8">
-        <div className="w-full md:w-8/12">
-          <section className="w-full mx-auto">
-            {blogs.map(blog => {
-              return <BlogPost key={blog.title} blog={blog} />
-            })}
-          </section>
+      <div className="fixed w-full h-72 sm:h-80 md:h-104 lg:h-120 xl:h-132 2xl:h-152">
+        <Image
+          src="/images/seoul-national-university.jpeg"
+          layout="fill"
+          objectFit="cover"
+          alt="seoul-national-university"
+          className=""
+        />
+      </div>
+
+      <section className="relative h-72 sm:h-80 md:h-104 lg:h-120 xl:h-132 2xl:h-152">
+        <div className="absolute w-full h-full bg-cyan-900 opacity-90"></div>
+        <div className="flex flex-col justify-center h-full items-center text-white">
+          <h1 className="z-10 flex flex-col justify-center whitespace-nowrap font-light text-7xl md:text-8xl xl:text-9xl mb-4 lg:mb-8">
+            GY Blog
+          </h1>
+          <h2 className="z-10 flex flex-col text-center text-xl xs:text-2xl sm:text-3xl md:text-4xl max-w-xs xs:max-w-sm sm:max-w-md mx-4 md:max-w-lg lg:w-full">
+            Read the latest from Global Youth staff and parents
+          </h2>
         </div>
-        <div className="md:flex flex-col items-stretch md:ml-10 lg:ml-20 md:w-4/12 xl:3/12 p-4 sm:p-0">
-          <section className="flex flex-col items-stretch w-full text-cyan-900 rounded-md mx-auto mb-10 ">
-            <h2 className="text-4xl mb-2">Search Blogs</h2>
-            <input
-              type="text"
-              placeholder="self-care"
-              className=" border-2 border-cyan-900 rounded px-2 w-full"
-            />
-          </section>
-          <section>
-            <h3 className="text-4xl mb-4 text-cyan-900">Popular Posts</h3>
-            <div className="w-full bg-cyan-900 p-4 rounded">
-              <h4 className="text-xl text-white mb-4 hover:cursor-pointer hover:text-cyan-100">
-                Practice Self Care For An Energy Boost
-              </h4>
-              <h4 className="text-xl text-white mb-4 hover:cursor-pointer hover:text-cyan-100">
-                Super Foods for Studying
-              </h4>
-              <h4 className="text-xl text-white mb-4 hover:cursor-pointer hover:text-cyan-100">
-                What College Admissions Staff Are Thinking
-              </h4>
-              <h4 className="text-xl text-white mb-4 hover:cursor-pointer hover:text-cyan-100">
-                4 Questions you should try and ask your student while away
-              </h4>
-              <h4 className="text-xl text-white mb-4 hover:cursor-pointer hover:text-cyan-100">
-                Covid 19- The Latest
-              </h4>
-            </div>
-          </section>
-          <section className="flex flex-col w-full my-12">
-            <h3 className="text-4xl mb-4 text-cyan-900 w-max">
-              Join Mailing List
-            </h3>
-            <input
-              type="email"
-              placeholder="yourEmail@yourProvider.com"
-              className=" border-2 rounded border-cyan-900 px-2 w-full"
-            />
-          </section>
+      </section>
+
+      <div className="relative w-full h-full bg-white z-10 pt-16">
+        <div className="flex flex-col md:flex-row justify-between w-full sm:w-11/12 max-w-screen-2xl mx-auto mt-8 lg:p-8">
+          <div className="w-full md:w-8/12">
+            <section className="w-full mx-auto">
+              {blogs.map(blog => {
+                return <BlogPost key={blog.title} blog={blog} />
+              })}
+            </section>
+          </div>
+          <div className="md:flex flex-col items-stretch md:ml-10 lg:ml-20 md:w-4/12 xl:3/12 p-4 sm:p-0">
+            <section className="flex flex-col items-stretch w-full text-cyan-900 rounded-md mx-auto mb-10 ">
+              <h2 className="md:text-2xl lg:text-3xl xl:text-4xl mb-2">
+                Search Blogs
+              </h2>
+              <input
+                type="text"
+                placeholder="self-care"
+                className=" border-2 border-cyan-900 px-2"
+              />
+            </section>
+            <section>
+              <h3 className="md:text-2xl lg:text-3xl xl:text-4xl mb-4 text-cyan-900">
+                Popular Posts
+              </h3>
+              <div className="w-full bg-cyan-900 p-4 rounded">
+                <h4 className="text-xl text-white mb-4 hover:cursor-pointer hover:text-cyan-100">
+                  Practice Self Care For An Energy Boost
+                </h4>
+                <h4 className="text-xl text-white mb-4 hover:cursor-pointer hover:text-cyan-100">
+                  Super Foods for Studying
+                </h4>
+                <h4 className="text-xl text-white mb-4 hover:cursor-pointer hover:text-cyan-100">
+                  What College Admissions Staff Are Thinking
+                </h4>
+                <h4 className="text-xl text-white mb-4 hover:cursor-pointer hover:text-cyan-100">
+                  4 Questions you should try and ask your student while away
+                </h4>
+                <h4 className="text-xl text-white mb-4 hover:cursor-pointer hover:text-cyan-100">
+                  Covid 19- The Latest
+                </h4>
+              </div>
+            </section>
+            <section className="flex flex-col w-full my-12">
+              <h3 className="md:text-2xl lg:text-3xl xl:text-4xl mb-4 text-cyan-900 w-max">
+                Join Mailing List
+              </h3>
+              <input
+                type="email"
+                placeholder="yourEmail@yourProvider.com"
+                className=" border-2 border-cyan-900 px-2 w-full"
+              />
+            </section>
+          </div>
         </div>
       </div>
     </>
