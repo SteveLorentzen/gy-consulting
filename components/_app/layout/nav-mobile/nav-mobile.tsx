c54ca7c8d6257a2cdfb621eba9d74a0a831aec03
Steve Lorentzen <Stevelorentzen@gmail.com>
2022-02-25 23:37:46 -0800

    added 'free consulting' button to nav and nav-mobile fixed home link on nav-mobile made it so the 'free consulting' button appears after scrolling down

--- components/_app/layout/nav-mobile/nav-mobile.tsx
+++ components/_app/layout/nav-mobile/nav-mobile.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import { DialogOverlay, DialogContent } from '@reach/dialog'
 
 import { BiMenu } from '@react-icons/all-files/bi/BiMenu'
+import { AiOutlineClose } from '@react-icons/all-files/ai/AiOutlineClose'
 
 import { navContent } from 'constants/_app/layout/constants-layout'
 import { useRouter } from 'next/router'
@@ -24,21 +25,36 @@ export function NavMobile() {
   return (
     <>
       <div
-        className="relative sm:hidden active:text-cyan-500"
+        className="relative md:hidden active:text-cyan-500 mr-4"
         onClick={() => (showDialog ? close() : open())}
       >
         <BiMenu className="text-6xl text-white" />
       </div>
 
       <DialogOverlay
-        className="flex sm:hidden justify-center pt-14 bg-cyan-800 min-h-screen w-full fixed top-0 left-0 z-40 animate-slidein "
+        className="flex md:hidden justify-center bg-cyan-800 min-h-screen w-full fixed top-0 left-0 z-50 animate-slidein "
         isOpen={showDialog}
         onDismiss={close}
       >
         <DialogContent
           aria-label="Navigation Menu"
-          className="relative flex flex-col justify-start items-start  min-w-full min-h-full px-16 tracking-wide text-3xl"
+          className="relative flex flex-col py-12 justify-start items-start  min-w-full min-h-full px-16 tracking-wide text-3xl"
         >
+          <Link href="/">
+            <a
+              className="absolute top-4 left-4  text-white text-5xl font-semibold tracking-wider"
+              onClick={() => setShowDialog(false)}
+            >
+              GY
+            </a>
+          </Link>
+          <div
+            className="absolute top-4 right-4 text-5xl text-white"
+            onClick={() => setShowDialog(false)}
+          >
+            <AiOutlineClose />
+          </div>
+
           <button
             onClick={navigateToLogin}
             className="mx-auto my-12 bg-white px-24 py-3 rounded-md text-cyan-900 active:text-cyan-700 font-bold "
@@ -59,6 +75,17 @@ export function NavMobile() {
                 </Link>
               )
             })}
+            <hr className="bg-white w-full my-10"></hr>
+            <div className="flex justify-center w-full ">
+              <button
+                onClick={() => {
+                  alert('get a free consultation!')
+                }}
+                className=" bg-orange-400 hover:bg-orange-500 text-white py-4 px-6 rounded-lg  whitespace-nowrap font-bold tracking-wide"
+              >
+                Free Consultation
+              </button>
+            </div>
           </div>
         </DialogContent>
       </DialogOverlay>
--- components/_app/layout/nav/nav.tsx
+++ components/_app/layout/nav/nav.tsx
@@ -2,25 +2,54 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 
 import { navContent } from 'constants/_app/layout/constants-layout'
+import { useEffect, useState } from 'react'
 
-export function Nav() {
+export function Nav({ yPosition }: { yPosition: number }) {
+  const [scheduleButtonIsVisible, setScheduleButtonIsVisible] = useState(false)
+  const [buttonWidth, setButtonWidth] = useState('')
+
   const router = useRouter()
 
+  useEffect(() => {
+    if (yPosition > 500 && !scheduleButtonIsVisible) {
+      setButtonWidth(`100%`)
+      setScheduleButtonIsVisible(true)
+    } else if (yPosition <= 500) {
+      setButtonWidth(`${(yPosition / 500) * 100}%`)
+      setScheduleButtonIsVisible(false)
+    }
+  }, [yPosition, scheduleButtonIsVisible])
+
+  console.log(buttonWidth)
+
   return (
-    <div className="hidden sm:flex w-152 sm:w-232 justify-end space-between items-center tracking-wider text-xl z-20">
-      {navContent.navLinks.map(navLink => {
-        return (
-          <Link key={navLink.linkText} href={navLink.destination}>
-            <a
-              className={` text-white hover:text-cyan-200 whitespace-nowrap ml-8 lg:ml-16 ${
-                router.pathname === navLink.destination ? 'text-cyan-200' : ''
-              }  `}
-            >
-              {navLink.linkText}
-            </a>
-          </Link>
-        )
-      })}
+    <div className="hidden md:flex justify-end space-between items-center tracking-wider text-xl z-20">
+      <div className="md:mr-4 lg:mr-6 xl:mr-8">
+        {navContent.navLinks.map(navLink => {
+          return (
+            <Link key={navLink.linkText} href={navLink.destination}>
+              <a
+                className={` text-white hover:text-cyan-200 whitespace-nowrap ml-6 sm:ml-4 md:ml-6 lg:ml-12 xl:ml-16 ${
+                  router.pathname === navLink.destination ? 'text-cyan-200' : ''
+                }  `}
+              >
+                {navLink.linkText}
+              </a>
+            </Link>
+          )
+        })}
+      </div>
+
+      <div style={{ width: buttonWidth }} className="overflow-hidden ">
+        <button
+          onClick={() => {
+            alert('get a free consultation!')
+          }}
+          className=" bg-orange-400 hover:bg-orange-500 text-white  py-2 px-6 rounded-lg  whitespace-nowrap font-bold tracking-wide mr-6 sm:ml-4 lg:ml-6 xl:ml-10"
+        >
+          Free Consultation
+        </button>
+      </div>
     </div>
   )
 }
