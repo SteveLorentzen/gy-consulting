9aefc0cb4bca57a5000f1d525b98d84bd377db31
Steve Lorentzen <Stevelorentzen@gmail.com>
2022-03-09 16:40:33 -0800

    created common call-to-action component worked on styling for home page while maintaining styling in other components

---
+++ components/common/call-to-action.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+
+export function CallToAction({
+  text,
+  frontPage,
+}: {
+  text: string
+  frontPage?: boolean
+}) {
+  return (
+    <>
+      <h2
+        className={`${
+          frontPage ? 'hidden' : 'flex'
+        } xs:flex items-center text-center text-xl xs:text-2xl sm:text-3xl md:text-4xl max-w-xs xs:max-w-sm sm:max-w-md mx-4 md:max-w-lg lg:w-full`}
+      >
+        {text}
+      </h2>
+      <Link href="/">
+        <a className="flex items-center  py-4 px-8 bg-orange-400 hover:bg-orange-500 text-white rounded-md font-bold whitespace-nowrap">
+          Free Consultation{' '}
+        </a>
+      </Link>
+    </>
+  )
+}
